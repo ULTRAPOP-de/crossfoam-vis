9391ce40431c5bf35e685f8e48b9e3a3da2fba91
Sebastian Meier <contact@sebastianmeier.eu>
2019-10-08 18:46:20 +0200

    experiment branch

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as d3 from "d3";
-import { ClusterVis } from "./cluster";
-import { ListVis } from "./list";
-import { NetworkVis } from "./network";
-import { OverviewVis } from "./overview";
-
-export { ClusterVis, ListVis, OverviewVis, NetworkVis};
